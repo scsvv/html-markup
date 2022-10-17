53f0660ee9dc7e68f4bfe7d52f845e656c067d35
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2022-10-17 22:04:31 +0300

    add popup menu

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import './style.css'
 import './styles/index.scss'
 
+const list = document.querySelectorAll('.movie__seanse_list');
+
+
+const days_list_popup = document.querySelector('.popup_menu')
+const days_popup_button = document.querySelector('.open_dayslist')
+
+console.log(days_popup_button, days_list_popup)
+document.addEventListener('click', (event) => {
+    if (event.target == days_popup_button) {
+        console.log("asd")
+        days_list_popup.classList.toggle('popup_menu_on')
+    }
+})
