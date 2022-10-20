ed7b004a6e6806ae30f3fdbcf76cbebe3c058118
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2022-10-20 20:00:15 +0300

    loging

--- main.js
+++ main.js
@@ -3,26 +3,21 @@ import './styles/index.scss'
 const days_list_popup = document.querySelector('.popup_menu')
 const days_popup_button = document.querySelector('.open_dayslist')
 
-console.log(days_popup_button, days_list_popup)
-document.addEventListener('click', (event) => {
-    if (event.target == days_popup_button) {
-        days_list_popup.classList.toggle('popup_menu_on')
-    };
-})
+days_popup_button.addEventListener('click', (event) => {
+      days_list_popup.classList.toggle('popup_menu_on')
+    });
 ; 
 
 const rightButtons = [...document.querySelectorAll('.right')];
 console.log(rightButtons);
 
 rightButtons.forEach(function ($button) {
-  //   console.log($button);
+
   $button.addEventListener('click', function () {
     let $closest = this.closest('.movie__scroll').closest(
       '.movie__description',
     );
-
     let $targetList = $closest.querySelector('.movie__seanse_list');
-
     console.log($targetList);
   });
 });
