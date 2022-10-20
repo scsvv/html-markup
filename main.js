2c0e8a8176ffa68ff9543cad88b19a4e29117ef0
Lyshaf <zvat.333333@gmail.com>
2022-10-20 17:21:57 +0300

    20.10 updating of scss

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-import './style.css'
 import './styles/index.scss'
-
-const list = document.querySelectorAll('.movie__seanse_list');
-
 
 const days_list_popup = document.querySelector('.popup_menu')
 const days_popup_button = document.querySelector('.open_dayslist')
@@ -10,7 +6,23 @@ const days_popup_button = document.querySelector('.open_dayslist')
 console.log(days_popup_button, days_list_popup)
 document.addEventListener('click', (event) => {
     if (event.target == days_popup_button) {
-        console.log("asd")
         days_list_popup.classList.toggle('popup_menu_on')
-    }
+    };
 })
+; 
+
+const rightButtons = [...document.querySelectorAll('.right')];
+console.log(rightButtons);
+
+rightButtons.forEach(function ($button) {
+  //   console.log($button);
+  $button.addEventListener('click', function () {
+    let $closest = this.closest('.movie__scroll').closest(
+      '.movie__description',
+    );
+
+    let $targetList = $closest.querySelector('.movie__seanse_list');
+
+    console.log($targetList);
+  });
+});
